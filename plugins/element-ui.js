b9882bd1483a8f471d1acb107e13613c90d5a743
Aidan Nichol <aidan@nicholware.co.uk>
2019-01-20 14:06:03 +0000

    removed redundant elements

--- plugins/element-ui.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-// import i18n from 'vue-i18n';
-import locale from 'element-ui/lib/locale/lang/en';
-import ElementUI from 'element-ui';
-// import 'element-ui/lib/theme-default/index.css';
-// Vue.use(i18n);
-Vue.use(ElementUI, {
-  locale
-});
